4d3dde9facb1eb52afc473226268bc1c3cd89a0b
Mdsoln <muddyfakih98@gmail.com>
2025-03-17 11:44:43 +0300

    backend apis initial ssetup

---
+++ api/src/main/java/com/lishe/LisheApplication.java
@@ -0,0 +1,13 @@
+package com.lishe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisheApplication.class, args);
+	}
+
+}
