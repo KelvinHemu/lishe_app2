2fd3f35199a3723c7aeea1bc9bd2e7b4359d4a03
Mdsoln <muddyfakih98@gmail.com>
2025-03-17 12:46:52 +0300

    feat(ui): fix jwt userDetails

---
+++ api/src/main/java/com/lishe/entity/UserRoles.java
@@ -0,0 +1,5 @@
+package com.lishe.entity;
+
+public enum UserRoles {
+    USER, ADMIN
+}
--- api/src/main/java/com/lishe/entity/Users.java
+++ api/src/main/java/com/lishe/entity/Users.java
@@ -3,6 +3,12 @@ package com.lishe.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -11,8 +17,47 @@ import lombok.*;
 @Setter
 @Getter
 @Builder
-public class Users {
+public class Users implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRoles roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roles.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return username;
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
